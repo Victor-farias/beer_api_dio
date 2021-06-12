3d3e9c0eee6a576e27da9a066017ef430e7b967a
Victor-farias <fariasvictor449@gmail.com>
2021-06-11 21:49:25 -0300

    Aplication part

---
+++ src/test/java/com/one/digital/innovation/beerstock/BeerApiDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one.digital.innovation.beerstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerApiDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
